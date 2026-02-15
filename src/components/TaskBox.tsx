406f2832d313ace945aa0badd683d40421789f88
gulbasozan <gulbasozan@gmail.com>
2026-02-15 21:10:36 +0300

    Add shadcn Dialouge for task manupilation

--- src/components/TaskBox.tsx
+++ src/components/TaskBox.tsx
@@ -1,4 +1,4 @@
-import { Circle } from "lucide-react";
+import { Circle, CopyPlus, SquareMinus, SquarePlus } from "lucide-react";
 
 import SubtaskBox from "./SubTaskBox";
 import AddTask from "./AddTask";
@@ -7,39 +7,68 @@ import DeleteButton from "./DeleteButton";
 import { deleteTask } from "../api/delete_task";
 import { useTasksAPI } from "../contexts/TasksProvider";
 
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 const TaskBox = ({ task }: { task: Task }) => {
     const { fetchTasks } = useTasksAPI();
 
     const handleDelete = () => {
-        deleteTask(task.id).then((res) => {
-            if (res.error) throw res.error;
-        }).then(() => fetchTasks()).catch(e => console.log(e))
-    }
+        deleteTask(task.id)
+            .then((res) => {
+                if (res.error) throw res.error;
+            })
+            .then(() => fetchTasks())
+            .catch((e) => console.log(e));
+    };
 
     return (
-        <div className="flex flex-col items-start justify-center w-full">
-            <div className="m-2 p-2 w-full">
-                <div className="flex flex-row justify-between items-center">
-                    <div className="flex flex-row gap-2 items-center justify-start">
-                        <Circle size={20} />
-                        <h1 className="font-bold text-2xl">{`${task.text} (${task.priority})`}</h1>
+        <DropdownMenu>
+            <div className="flex flex-col items-start justify-center w-full">
+                <div className="m-2 p-2 w-full">
+                    <div className="flex flex-row justify-between items-center">
+                        <DropdownMenuTrigger asChild>
+                            <div className="flex flex-row gap-2 items-center justify-start">
+                                <Circle size={20} />
+                                <h1 className="font-bold text-2xl">
+                                    {task.text}
+                                </h1>
+                            </div>
+                        </DropdownMenuTrigger>
                     </div>
-                    <DeleteButton handleClick={handleDelete} />
+                    <DropdownMenuContent>
+                        <DropdownMenuItem>
+                            <SquarePlus /> Task
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                            <CopyPlus /> Task Context
+                        </DropdownMenuItem>
+                        <DropdownMenuItem
+                            variant="destructive"
+                            onClick={handleDelete}
+                        >
+                            <SquareMinus /> Delete Task
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                    <div className="flex flex-col gap-2">
+                        {task.subtasks.length > 0 &&
+                            task.subtasks.map((subtask: Subtask) => (
+                                <SubtaskBox
+                                    key={subtask.id}
+                                    subtask={subtask}
+                                    taskID={task.id}
+                                />
+                            ))}
+                        <AddSubtask taskID={task.id} />
+                    </div>
                 </div>
-                <div className="flex flex-col gap-2">
-                    {task.subtasks.length > 0 &&
-                        task.subtasks.map((subtask: Subtask) => (
-                            <SubtaskBox
-                                key={subtask.id}
-                                subtask={subtask}
-                                taskID={task.id}
-                            />
-                        ))}
-                    <AddSubtask taskID={task.id} />
-                </div>
+                <AddTask taskPriority={task.priority} />
             </div>
-            <AddTask taskPriority={task.priority} />
-        </div>
+        </DropdownMenu>
     );
 };
 
